88fa5275c4319f6d703a0d87b30a277182d3a7bf
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-28 16:57:53 +0300

    Обычный подсчёт

---
+++ src/main/java/one/Calculator.java
@@ -0,0 +1,24 @@
+package one;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public long sum(int[] array) {
+        int sum = 0;
+        for (int j : array) {
+            sum += j;
+        }
+        return sum;
+    }
+
+    public double average(int[] array) {
+        long sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum / array.length;
+    }
+
+}
